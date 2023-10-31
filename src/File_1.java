2a7d06183ef36351684105c879187b67f40307d0
AdityaVshk100 <AdityaVshk100@example.com>
2023-10-31 10:12:37 +0530

    Practise

--- src/File_1.java
+++ src/File_1.java
@@ -1,2 +1,12 @@
+
+import java.util.Scanner;
 public class File_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        System.out.println("Hello World");
+        System.out.print("Afditya");
+
+    }
 }
