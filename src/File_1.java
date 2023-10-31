32cb449a52fdef4ea4ba28306eb0f6ca8eb6343c
AdityaVshk100 <AdityaVshk100@example.com>
2023-10-31 19:14:30 +0530

    Practise

--- src/File_1.java
+++ src/File_1.java
@@ -1,16 +1,24 @@
-
 import java.util.Scanner;
 public class File_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /*
         int a = sc.nextInt();
         System.out.println(a);
         System.out.println("Hello World");
         System.out.print("Afditya");
 
-        System.out.print("Afditya");
-        System.out.print("Afditya");
-        System.out.print("Afditya");
+         */
+        System.out.print(10);
+        System.out.print(20);
+        System.out.print(30);
+        System.out.println(40);
+        System.out.println(50);
+
+        System.out.println("ALgoPrep");
+
+
+
 
     }
 }
---
+++ src/File_2.java
@@ -0,0 +1,7 @@
+public class File_2 {
+    public static void main(String[] args) {
+        System.out.println("7*10");
+        System.out.println(7*10+"Hello");
+        System.out.println("Hello"+7*10);
+    }
+}
